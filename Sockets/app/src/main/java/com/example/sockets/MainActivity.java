4319f011c72f13c390bf278deb16afb1954f9cd9
jorge <donjuan.jfc@gmail.com>
2020-05-09 22:44:57 +0200

    Create view  for enter data user

--- Sockets/app/src/main/java/com/example/sockets/MainActivity.java
+++ Sockets/app/src/main/java/com/example/sockets/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.sockets;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +9,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(getSupportActionBar() != null)
+            getSupportActionBar().hide();
+
     }
 }
