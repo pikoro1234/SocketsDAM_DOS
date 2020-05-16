2244b9daef9092870f6aa6e40a2bc7e09b959cea
jorge <donjuan.jfc@gmail.com>
2020-05-16 17:20:51 +0200

    complet chat server and client version 1.5

--- Sockets/app/src/main/java/com/example/sockets/MainActivity.java
+++ Sockets/app/src/main/java/com/example/sockets/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.sockets;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ConnectException;
 import java.net.Socket;
@@ -18,11 +22,9 @@ public class MainActivity extends AppCompatActivity {
 
     private Button botonEnviar,botonConectar,botonDesconectar;
 
-    private boolean suitAbrirConexion,suitCerrarConexion;
+    private String fecha,hora,recibido;
 
     private Socket socket;
-
-    String fecha,hora;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,40 +57,10 @@ public class MainActivity extends AppCompatActivity {
         botonConectar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
 
-                        try {
+                Conectar conectar = new Conectar();
 
-                            suitAbrirConexion = true;
-
-                            socket = new Socket(txtipUser.getText().toString(), Integer.parseInt(txtpuertoConexion.getText().toString()));
-
-                            System.out.println("LA CONEXION CORRECTA "+txtipUser.getText().toString()+" "+Integer.parseInt(txtpuertoConexion.getText().toString()));
-
-                        } catch (ConnectException exception) {
-
-                            System.out.println("PRIMER CATCH");
-
-                            suitAbrirConexion = false;
-
-                        } catch (IOException e) {
-
-                            System.out.println("SEGUNDO CATCH");
-
-                        }
-                    }
-                }).start();
-
-
-                if (suitAbrirConexion){
-
-                    txtBandejaEntrada.setText(hora+" conexion establecida\n");
-                }else{
-
-                    showMessage("NO SE ESTABLECIO CONEXIÓN");
-                }
+                conectar.execute();
             }
         });
 
@@ -96,42 +68,139 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                showMessage("MENSAJE ENVIADO");
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        DataInputStream in;
+
+                        DataOutputStream out;
+
+                        try {
+
+                            while (true){
+
+                                in = new DataInputStream(socket.getInputStream());
+
+                                out = new DataOutputStream(socket.getOutputStream());
+
+                                out.writeUTF(txtmensajeUser.getText().toString());
+
+                                recibido = in.readUTF();
+
+                                System.out.println(recibido);
+
+                                Conectar con = new Conectar();
+
+                                con.execute();
+
+                            }
+
+                        } catch (IOException e) {
+
+                            e.printStackTrace();
+                        }
+
+                    }
+                }).start();
+
+                txtBandejaEntrada.setText(txtmensajeUser.getText().toString());
+
             }
         });
 
         botonDesconectar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
 
-                        try {
+                Desconectar desconectar = new Desconectar();
 
-                            suitCerrarConexion = true;
+                desconectar.execute();
 
-                            socket.close();
-
-                            System.out.println("LA CONEXION SE CERRO ");
-
-                        } catch (IOException e) {
-
-                            e.printStackTrace();
-                        }
-                    }
-                }).start();
-
-                if (suitCerrarConexion){
-
-                    txtBandejaEntrada.setText(hora+" conexión cerrada");
-                }
             }
         });
     }
 
+
+
+    class Conectar extends AsyncTask<String,Void,Void>{
+
+        boolean interruptor;
+
+        @Override
+        protected void onPreExecute() {
+
+        }
+
+        @SuppressLint("WrongThread")
+        @Override
+        protected Void doInBackground(String... strings) {
+
+            try {
+
+                socket = new Socket(txtipUser.getText().toString(),Integer.parseInt(txtpuertoConexion.getText().toString()));
+
+                interruptor = true;
+            } catch (ConnectException e) {
+
+                e.printStackTrace();
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            if (interruptor){
+                showMessage("CONECTAMOS");
+                txtBandejaEntrada.setText(fecha+" conectamos");
+            }else{
+                showMessage("NO CONECTAMOS");
+            }
+
+        }
+    }
+
+    class Desconectar extends AsyncTask<String,Void,Void>{
+
+        @Override
+        protected void onPreExecute() {
+
+            showMessage("DESCONECTADO");
+
+            txtBandejaEntrada.setText("desconectado");
+        }
+
+        @Override
+        protected Void doInBackground(String... strings) {
+
+            try {
+
+                socket.close();
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+        }
+    }
+
     //metodo atajo para el toast vista usuario
-    protected void showMessage(String message){
+    public void showMessage(String message){
 
         Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
     }
